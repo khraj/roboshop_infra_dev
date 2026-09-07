9ffe690f2e13491dd6612f667c548b8a9563463d
khraj <krishnakoochetti57@gmail.com>
2026-09-07 16:57:58 +0530

    yterraform

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
+component = $1
 dnf install ansible -y
-ansible-pull -U https://github.com/khraj/ansible-roboshop-roles-tf.git -e component=mongodb main.yaml
+ansible-pull -U https://github.com/khraj/ansible-roboshop-roles-tf.git -e component=$component main.yaml
