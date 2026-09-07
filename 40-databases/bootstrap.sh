3780eb67295e6c8616b793ab21f6543590129819
khraj <krishnakoochetti57@gmail.com>
2026-09-07 18:07:02 +0530

    added files

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
 component=$1
 dnf install ansible -y
-ansible-pull -U https://github.com/khraj/ansible-roboshop-roles-tf.git -e component=$component main.yaml
+#ansible-pull -U https://github.com/khraj/ansible-roboshop-roles-tf.git -e component=$component main.yaml
+
+
+REPO_URL = https://github.com/khraj/ansible-roboshop-roles-tf.git
+REPO_DIR = /opt/roboshop/ansible
+ANSIBLE_DIR = ansible-roboshop-roles-tf
+
+
+mkdir -p $REPO_DIR
+mkdir -p /var/log/roboshop
+touch /var/log/roboshop/ansible.log
+cd $REPO_DIR
+if [ ! -d "$ANSIBLE_DIR" ]; then
+    git clone $REPO_URL
+    cd $ANSIBLE_DIR
+else
+    cd $ANSIBLE_DIR
+    git pull
+fi
+
+ansible-playbook -e component=$component main.yaml
--- 40-databases/data.tf
+++ 40-databases/data.tf
@@ -30,6 +30,10 @@ data "aws_ssm_parameter" "rabbitMQ_sg_id" {
   name = "/${var.project_name}/${var.environment}/rabbitMQ_sg_id"
 }
 
+data "aws_ssm_parameter" "mysql_sg_id" {
+  name = "/${var.project_name}/${var.environment}/mysql_sg_id"
+}
+
 data "aws_ssm_parameter" "public_subnet_ids" {
   name = "/${var.project_name}/${var.environment}/public_subnet_ids"
 }
