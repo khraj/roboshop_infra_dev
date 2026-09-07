ad2d89749bd88bdedad637ac6f413fe9016da404
khraj <krishnakoochetti57@gmail.com>
2026-09-07 19:13:58 +0530

    ss

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -33,7 +33,7 @@ resource "terraform_data" "mongodb" {
     inline = [
         "chmod +x /tmp/bootstrap.sh",
         /* "sudo sh /tmp/bootstrap.sh" */
-        "sudo sh /tmp/bootstrap.sh mongodb"
+        "sudo sh /tmp/bootstrap.sh mongodb dev"
     ]
   }
 
@@ -75,7 +75,7 @@ resource "terraform_data" "redis" {
     inline = [
         "chmod +x /tmp/bootstrap.sh",
         /* "sudo sh /tmp/bootstrap.sh", */
-        "sudo sh /tmp/bootstrap.sh redis"
+        "sudo sh /tmp/bootstrap.sh redis dev"
     ]
   }
 
@@ -117,7 +117,7 @@ resource "terraform_data" "rabbitmq" {
     inline = [
         "chmod +x /tmp/bootstrap.sh",
         /* "sudo sh /tmp/bootstrap.sh", */
-        "sudo sh /tmp/bootstrap.sh rabbitmq"
+        "sudo sh /tmp/bootstrap.sh rabbitmq dev"
     ]
   }
 
