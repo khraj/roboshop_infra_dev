c7c9be165b83220b199784f59929c7f438b6aed5
khraj <krishnakoochetti57@gmail.com>
2026-09-07 18:58:55 +0530

    added files

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 component=$1
+environment=$2
 dnf install ansible -y
 #ansible-pull -U https://github.com/khraj/ansible-roboshop-roles-tf.git -e component=$component main.yaml
 
@@ -21,4 +22,4 @@ else
     git pull
 fi
 
-ansible-playbook -e component=$component main.yaml
+ansible-playbook -e component=$component environment=$environment main.yaml
--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -122,3 +122,50 @@ resource "terraform_data" "rabbitmq" {
   }
 
 }
+
+
+resource "aws_instance" "mysql" {
+  ami                    = local.ami_id
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [local.mysql_sg_id]
+  subnet_id              = local.database_subnet_id
+  iam_instance_profile   = aws_iam_instance_profile.mysql.name
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${local.common_name_suffix}-mysql"
+    }
+  )
+}
+
+resource "aws_iam_instance_profile" "mysql" {
+  name = "mysql"
+  role = "ec2_ssm_parameter_read"
+}
+
+resource "terraform_data" "mysql" {
+  triggers_replace = [
+    aws_instance.mysql.id
+  ]
+
+  connection {
+    type        = "ssh"
+    host        = aws_instance.mysql.private_ip
+    user        = "ec2-user"
+    password    = "DevOps321"
+  }
+  #terraform copeies this file to mysql server
+  provisioner "file" {
+    source      = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+        "chmod +x /tmp/bootstrap.sh",
+        /* "sudo sh /tmp/bootstrap.sh", */
+        "sudo sh /tmp/bootstrap.sh mysql dev"
+    ]
+  }
+
+}
