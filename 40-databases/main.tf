783b1fcb0bfe1574afeaf924f8883233ac88c4d6
khraj <krishnakoochetti57@gmail.com>
2026-09-08 15:16:09 +0530

    added files

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -176,7 +176,7 @@ resource "aws_route53_record" "mongodb" {
   name    = "mongodb-${var.environment}.${var.domain_name}"
   type    = "A"
   ttl     = 1
-  records = ["aws_instance.mongodb.private_ip"]
+  records = [aws_instance.mongodb.private_ip]
   allow_overwrite = true
 }
 
@@ -185,7 +185,7 @@ resource "aws_route53_record" "redis" {
   name    = "redis-${var.environment}.${var.domain_name}"
   type    = "A"
   ttl     = 1
-  records = ["aws_instance.redis.private_ip"]
+  records = [aws_instance.redis.private_ip]
   allow_overwrite = true
 }
 
@@ -194,7 +194,7 @@ resource "aws_route53_record" "rabbitmq" {
   name    = "rabbitmq-${var.environment}.${var.domain_name}"
   type    = "A"
   ttl     = 1
-  records = ["aws_instance.rabbitmq.private_ip"]
+  records = [aws_instance.rabbitmq.private_ip]
   allow_overwrite = true
 }
 
@@ -203,6 +203,6 @@ resource "aws_route53_record" "mysql" {
   name    = "mysql-${var.environment}.${var.domain_name}"
   type    = "A"
   ttl     = 1
-  records = ["aws_instance.mysql.private_ip"]
+  records = [aws_instance.mysql.private_ip]
   allow_overwrite = true
 }
