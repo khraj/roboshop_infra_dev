29efbc6967d92b39762409f10b3814615aa8476c
khraj <krishnakoochetti57@gmail.com>
2026-09-08 16:05:42 +0530

    added

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -71,4 +71,14 @@ resource "aws_security_group_rule" "catalogue_bastion" {
 
 }
 
+resource "aws_security_group_rule" "mongodb_catalogue" {
+  type                     = "ingress"
+  security_group_id        = local.mongodb_sg_id
+  source_security_group_id = local.catalogue_sg_id
+  from_port                = 27017
+  to_port                  = 27017
+  protocol                 = "tcp"
+
+}
+
 
