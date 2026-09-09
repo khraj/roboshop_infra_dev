806388767dbc420c1a2de195615ffaf5ecd89a77
khraj <krishnakoochetti57@gmail.com>
2026-09-09 15:52:41 +0530

    added

--- 60-catalog/locals.tf
+++ 60-catalog/locals.tf
@@ -1,6 +1,6 @@
 locals {
   catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
-  private_subnet_ids = split("," , data.aws_ssm_parameter.private_subnet_ids.value)[0]
+  private_subnet_ids = split("," , data.aws_ssm_parameter.private_subnet_ids.value)
   ami_id = data.aws_ami.krishna.id
   vpc_id = data.aws_ssm_parameter.vpc_id.value
   common_name_suffix = "${var.project_name}-${var.environment}"
